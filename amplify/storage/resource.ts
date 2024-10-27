1a3fd2fd5d9c44de0f81435966a827d1e525feb9
kailunh <kailunhong94@gmail.com>
2024-10-26 20:10:50 -0700

    use amplifyu uplader

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,8 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive',
+  name: 'form-files',
   access: (allow) => ({
-    'pdf/{entity_id}/*':allow.entity('identity').to(['read', 'write', 'delete'])
+    'forms/{entity_id}/*': [
+      // {entity_id} is the token that is replaced with the user identity id
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
   })
 });
